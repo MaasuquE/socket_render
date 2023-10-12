25b8f0ff12f8e9f319ccc4fdf5de2ef53583b5c5
MaasuquE <mdmaasuque@gmail.com>
2023-10-12 16:20:25 +0100

    ypdateed

--- routes/auth.js
+++ routes/auth.js
@@ -2,29 +2,15 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
-// Create a dictionary to store tokens by domain
-const domainTokens = {};
-
 router.get('/generate-token', (req, res) => {
   const domain = req.query.domain;
-
+  console.log(domain);
   if (!domain) {
     return res.status(400).json({ error: 'Domain is required' });
   }
 
-  // Check if a token already exists for the domain
-  if (domainTokens[domain]) {
-    const token = domainTokens[domain];
-    res.json({ token });
-  } else {
-    // Generate a new token for the domain
-    const token = jwt.sign({ domain }, process.env.JWT_SECRET);
-
-    // Store the token for the domain
-    domainTokens[domain] = token;
-
-    res.json({ token });
-  }
+  const token = jwt.sign({ domain }, process.env.JWT_SECRET);
+  res.json({ token });
 });
 
 module.exports = router;
